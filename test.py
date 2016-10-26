1efcb4a37a998901a3f6597563f2bf19338def0f
xiaobing <xiaobingpang@qq.com>
2016-10-26 10:00:45 +0800

    1. test.py

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
 import pytdxtrader
+#创建user
 user = pytdxtrader.TdxTrader('1234','hbzq')
+#当日委托
+print(user.update_order()) 
+#当日成交
+print(user.update_trade())
+#发单, 参数(合约代码，方向，价格，数量)
+print(user.send_order('600000',0,10,100))
+#撤单,参数指定委托号
+#user.cancel_order('2881')
